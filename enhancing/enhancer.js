c8409270536b7ab2bb64974e27a486a512078a3a
tarik-84 <niceloveman200@yahoo.com>
2020-07-15 14:31:47 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,39 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement === 20){
+    return { ...item }
+  }else {
+    return {...item, enhancement: item.enhancement + 1}
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    return {...item, durability: item.durability - 5}
+  }
+  if(item.enhancement >= 15) {
+    if (item.enhancement > 16) {
+      return {
+        ...item,
+        durability: item.durability - 10,
+        enhancement: item.enhancement - 1,
+      };
+    } else {
+      return { ...item, durability: item.durability - 10 };
+    }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: item.durability = 100 };
 }
 
 function get(item) {
-  return { ...item };
+  if(item.enhancement === 0){
+    return { ...item }
+  }
+  // if(item.name > 0){
+  //   return {...item, name: item.name}
+  // }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,124 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('repair', () => {
+    it('Restore durability to 100', () => {
+        const {repair} = enhancer
+
+        const obj = {
+            name: 'John',
+            durability: 100,
+            enhancement: 0,
+        }
+        const newObj  = repair(obj)
+
+        expect(newObj.durability).toBe(100)
+    })
+})
+
+describe('succeed', () => {
+    it('should increase enhancement level by 1', () => {
+        const {succeed} = enhancer
+        
+        const obj = {
+            name: 'John',
+            durability: 60,
+            enhancement: 0,
+        }
+        const newObj = succeed(obj)
+
+        expect(newObj.enhancement).toBe(1)
+    })
+    it('The enhancement level should not change at level 20', () => {
+        const {succeed} = enhancer
+        
+        const obj = {
+            name: 'John',
+            durability: 60,
+            enhancement: 20,
+        }
+        const newObj = succeed(obj)
+
+        expect(newObj.enhancement).toBe(20)
+    })
+    it('The durability should not change', () => {
+        const {succeed} = enhancer
+        
+        const obj = {
+            name: 'John',
+            durability: 60,
+            enhancement: 20,
+        }
+        const newObj = succeed(obj)
+
+        expect(newObj.durability).toBe(60)
+    })
+});
+
+describe('fail', () => {
+    it('If enhancement is < 15, durability decreases by 5', () => {
+        const {fail} = enhancer
+
+        const obj = {
+            name: 'John',
+            durability: 60,
+            enhancement: 14,
+        }
+        const newObj = fail(obj)
+
+        expect(newObj.durability).toBe(55)
+    })
+    it('If enhancement is >= 15, durability is decreased by 10', () => {
+        const {fail} = enhancer
+
+        const obj = {
+            name: 'John',
+            durability: 60,
+            enhancement: 15,
+        }
+        const newObj = fail(obj)
+
+        expect(newObj.durability).toBe(50)
+    })
+    it('If enhancement is > 16, the enhancement level decreases by 1', () => {
+        const {fail} = enhancer
+
+        const obj = {
+            name: 'John',
+            durability: 60,
+            enhancement: 17,
+        }
+        const newObj = fail(obj)
+
+        expect(newObj.enhancement).toBe(16)
+    })
+})
+
+describe('get', () => {
+    it('If the enhancement is 0, the the name is not modified', () => {
+        const {get} = enhancer
+
+        const obj = {
+            name: 'John',
+            durability: 60,
+            enhancement: 0,
+        }
+
+        const newObj = get(obj)
+
+        expect(newObj.name).toBeTruthy()
+    })
+    // it('If the enhancement is > 0, change the name to include the enhancement level', () => {
+    //     const {get} = enhancer
+
+    //     const obj = {
+    //         name: 'John',
+    //         durability: 60,
+    //         enhancement: 1,
+    //     }
+
+    //     const newObj = get(obj)
+
+    //     expect(newObj.name).toBeTruthy()
+    // })
+})
+
